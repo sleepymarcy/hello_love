af631e9221bcac0896704ac71407850ee6d05a1f
sleepymarcy <martyna.sowinska@hotmail.com>
2023-01-15 14:55:56 +0100

    moving and resizing rectangle

--- main.lua
+++ main.lua
@@ -3,6 +3,7 @@
 -- stuff u want to be alive for the whole program
 -- should be declared as local, before love.load()
 local important_variable
+local rectangle_coordinates
 -- ACHTUNG! declared, not initialised.
 -- better to let love decide when to initialise stuff
 
@@ -17,6 +18,11 @@ function love.load()
     
     local variable = 3 -- this has no effect outside this function
     important_variable = 13 -- this is going to be alive later.
+
+    rectangle_coordinates = {
+        10, 10,  -- x, y of top left corner
+        100, 50 -- width and height of the rectangle
+    }
 end
 
 -- love handles game loop for you.
@@ -25,8 +31,22 @@ end
 -- this should only contain stuff that are to be done in-between frames
 -- like handle logic, input etc
 function love.update(dt)
+    -- let's make the rectangle move slowly
+    rectangle_coordinates[1] = rectangle_coordinates[1] + 1
+    rectangle_coordinates[2] = rectangle_coordinates[2] + 1
+
+    -- these guys below are ACTUALY!!!!! width and height
+    -- so rectangle will resize
+    rectangle_coordinates[3] = rectangle_coordinates[3] + 1
+    rectangle_coordinates[4] = rectangle_coordinates[4] + 1
+    -- very stupid way to do it, but well, u have to start with something :)
 end
 
 -- and here you just build your frame
 function love.draw()
+    love.graphics.rectangle(
+        "fill",
+        rectangle_coordinates[1], rectangle_coordinates[2],
+        rectangle_coordinates[3], rectangle_coordinates[4]
+    )
 end
