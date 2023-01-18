a3bc85849b51c1cc70d3fb4ee7e48c8a22bc9bb4
sleepymarcy <martyna.sowinska@hotmail.com>
2023-01-18 22:52:16 +0100

    world-update

--- main.lua
+++ main.lua
@@ -6,5 +6,12 @@ local player_shape = love.physics.newRectangleShape(20,60)
 local player_body = love.physics.newBody(my_world, player_position_x, player_position_y, "dynamic")
 local player_fixture = love.physics.newFixture(player_body, player_shape)
 
+
+
+
+function love.update(dt)
+    my_world:update(dt)
+end
+
 function love.draw()
 end
