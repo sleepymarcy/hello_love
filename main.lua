b76a00a2fceeb2b65ef7e631ab323b7cd8e369c9
sleepymarcy <martyna.sowinska@hotmail.com>
2023-01-18 22:58:05 +0100

    player - properly drawn

--- main.lua
+++ main.lua
@@ -8,12 +8,13 @@ local player_shape = love.physics.newRectangleShape(u, 3 * u)
 local player_body = love.physics.newBody(my_world, player_position_x, player_position_y, "dynamic")
 local player_fixture = love.physics.newFixture(player_body, player_shape)
 
-
-
+local player_vertices
 
 function love.update(dt)
     my_world:update(dt)
+    player_vertices = {player_body:getWorldPoints(player_shape:getPoints())}
 end
 
 function love.draw()
+    love.graphics.polygon("fill", player_vertices)
 end
