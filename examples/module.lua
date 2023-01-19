421e1f1a5acaff15530b864e5ac3e79a2cd4cc9a
suap-dev <suap.dev@gmail.com>
2023-01-19 14:12:52 +0100

    get module.lua back

---
+++ examples/module.lua
@@ -0,0 +1,26 @@
+local module = {}
+
+module.test = {
+    "a",
+    ar = function()
+        print("arrrrr")
+        return "pirat", 12
+    end,
+    23,
+    function()
+        print("hajo")
+        return "siusiak"
+    end
+}
+
+module[4] = "cztery"
+module[12] = "12"
+module.dzialaj = function(x)
+    return x + 3
+end
+
+function module.dzialaj_tez(z)
+    return z * z
+end
+
+return module
