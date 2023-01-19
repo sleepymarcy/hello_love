3a5b3de9670f9dc9e24c25aa281f325833bb2709
suap-dev <suap.dev@gmail.com>
2023-01-19 22:04:02 +0100

    make it nicer

--- examples/structurisation/game/objects.lua
+++ examples/structurisation/game/objects.lua
@@ -2,9 +2,9 @@ local m = love.physics.getMeter()
 
 local base_color = { love.graphics.getColor() }
 local ball = {
-    body = love.physics.newBody(WORLD, 2*m, 2*m, "dynamic"),
-    shape = love.physics.newCircleShape(1*m),
-    color = { 1, 0, 0 },
+    body = love.physics.newBody(WORLD, 2 * m, 2 * m, "dynamic"),
+    shape = love.physics.newCircleShape(1 * m),
+    color = { 0.76, 0.18, 0.05 },
     draw = function(self)
         love.graphics.setColor(self.color)
         love.graphics.circle("fill", self.body:getX(), self.body:getY(), self.shape:getRadius())
@@ -13,20 +13,21 @@ local ball = {
 }
 ball.fixture = love.physics.newFixture(ball.body, ball.shape)
 
-local floor_height = 1*m;
+local floor_height = 1 * m;
 local floor = {
-    body = love.physics.newBody(WORLD, love.graphics.getWidth() / 2, love.graphics.getHeight() - floor_height/2, "static"),
+    body = love.physics.newBody(WORLD, love.graphics.getWidth() / 2, love.graphics.getHeight() - floor_height / 2,
+        "static"),
     shape = love.physics.newRectangleShape(love.graphics.getWidth(), floor_height),
-    color = { 0, 1, 0 },
+    color = { 0.28, 0.63, 0.05 },
     draw = function(self)
         love.graphics.setColor(self.color)
-        love.graphics.polygon("fill", {self.body:getWorldPoints(self.shape:getPoints())})
+        love.graphics.polygon("fill", { self.body:getWorldPoints(self.shape:getPoints()) })
         love.graphics.setColor(base_color)
-        
+
     end
-    
+
 }
 floor.fixture = love.physics.newFixture(floor.body, floor.shape)
 
 
-return {ball = ball, floor = floor}
+return { ball = ball, floor = floor }
--- examples/structurisation/main_ish.lua
+++ examples/structurisation/main_ish.lua
@@ -1,6 +1,7 @@
 -- init world and basic physics
 local meter = 20
 love.physics.setMeter(meter)
+love.graphics.setBackgroundColor(0.41, 0.53, 0.97)
 WORLD = love.physics.newWorld(0, 9.81 * meter, true)
 
 local ball = require("examples.structurisation.game.objects").ball
