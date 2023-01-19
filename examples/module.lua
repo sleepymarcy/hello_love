902d7297efa46b73162919468dccf7ec1e923bf9
suap-dev <suap.dev@gmail.com>
2023-01-19 13:31:38 +0100

    clear the examples

--- examples/module.lua
+++ examples/module.lua
@@ -1,26 +1,42 @@
-local module = {}
+-- init world and basic physics
+local m = 64
+local world = love.physics.newWorld(0, 9.81 * m, true)
+love.physics.setMeter(m)
 
-module.test = {
-    "a",
-    ar = function()
-        print("arrrrr")
-        return "pirat", 12
-    end,
-    23,
-    function()
-        print("hajo")
-        return "siusiak"
+local base_color = { love.graphics.getColor() }
+
+local ball = {
+    body = love.physics.newBody(world, 40, 40, "dynamic"),
+    shape = love.physics.newCircleShape(20),
+    color = { 1, 0, 0 },
+    draw = function(self)
+        love.graphics.setColor(self.color)
+        love.graphics.circle("fill", self.body:getX(), self.body:getY(), self.shape:getRadius())
+        love.graphics.setColor(base_color)
     end
 }
+ball.fixture = love.physics.newFixture(ball.body, ball.shape)
 
-module[4] = "cztery"
-module[12] = "12"
-module.dzialaj = function(x)
-    return x + 3
+local floor_height = 10;
+local floor = {
+    body = love.physics.newBody(world, love.graphics.getWidth() / 2, love.graphics.getHeight() - floor_height/2, "static"),
+    shape = love.physics.newRectangleShape(love.graphics.getWidth(), floor_height),
+    color = { 0, 1, 0 },
+    draw = function(self)
+        love.graphics.setColor(self.color)
+        love.graphics.polygon("fill", {self.body:getWorldPoints(self.shape:getPoints())})
+        love.graphics.setColor(base_color)
+        
+    end
+    
+}
+floor.fixture = love.physics.newFixture(floor.body, floor.shape)
+
+function love.draw()
+    ball:draw()
+    floor:draw()
 end
 
-function module.dzialaj_tez(z)
-        return z*z
+function love.update(dt)
+    world:update(dt)
 end
-
-return module
