024ae2ce870a6cf522c2482be82619d21560bcec
suap-dev <suap.dev@gmail.com>
2023-01-19 21:52:58 +0100

    update conf

--- conf.lua
+++ conf.lua
@@ -1,7 +1,8 @@
 function love.conf(t)
+    t.window.title = "Platformer"
     t.window.width = 800
     t.window.height = 400
-    t.window.title = "Platformer"
+    t.window.msaa = 8
     t.window.resizable = false
     t.window.fullscreen = false
     t.modules.timer = true
