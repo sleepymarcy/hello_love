a3edbaff5d70042227243967a47775cb894780fd
sleepymarcy <martyna.sowinska@hotmail.com>
2023-01-19 15:19:45 +0100

    it's alive!.. kinda

--- main.lua
+++ main.lua
@@ -3,17 +3,22 @@ WORLD = love.physics.newWorld(0, 9.81 * U) -- ... we set gravity to 9.81*u
 
 -- local player = require("player")
 local Platform = require("platform")
+local Player = require("player")
 
 local platform_1 = Platform.new()
 local platform_2 = Platform.new(150, 150)
 
+local player_1 = Player.new()
+
 function love.update(dt)
     WORLD:update(dt)
+    player_1.handle_input()
 end
 
 function love.draw()
     platform_1.draw()
     platform_2.draw()
+    player_1.draw()
 
     -- pr.player.draw()
     -- pm.platform.draw()
--- player.lua
+++ player.lua
@@ -1,26 +1,56 @@
-local module = {}
+local default_love_color = { love.graphics.getColor() }
 
-local player_position_x = 2 * U
-local player_position_y = 2 * U
-
-local default_color = { love.graphics.getColor() }
-
-
-module.player = {
-    shape = love.physics.newRectangleShape(U, 3 * U),
-    body = love.physics.newBody(WORLD, player_position_x, player_position_y, "dynamic"),
-    color = { 0, 1, 0 }
+local player = {}
+local default_player_color = { 1, 0, 1 }
+local default_keys = {
+    left = "left",
+    right = "right",
+    jump = "space"
 }
-module.player.fixture = love.physics.newFixture(module.player.body, module.player.shape)
 
-module.player.vertices = function()
-    return { module.player.body:getWorldPoints(module.player.shape:getPoints()) }
+function player.new(x, y, width, height, color, keys)
+    local new_player = {}
+
+    new_player.color = color or default_player_color
+    x = x or (10 * U)
+    y = y or (0 * U)
+    width = width or (1 * U)
+    height = height or (3 * U)
+    keys = keys or default_keys
+
+
+    new_player.shape = love.physics.newRectangleShape(width, height)
+    new_player.body = love.physics.newBody(WORLD, x, y, "dynamic")
+    new_player.fixture = love.physics.newFixture(new_player.body, new_player.shape)
+
+    function new_player.vertices()
+        return { new_player.body:getWorldPoints(new_player.shape:getPoints()) }
+    end
+
+    function new_player.handle_input()
+        if love.keyboard.isDown(keys.left) then
+            -- go left
+            new_player.body:setLinearVelocity(-20 * U, 0)
+        end
+
+        if love.keyboard.isDown(keys.right) then
+            -- go right
+            new_player.body:setLinearVelocity(20 * U, 0)
+        end
+
+        if love.keyboard.isDown(keys.jump) then
+            -- jump
+            new_player.body:setLinearVelocity(0, -20 * U)
+        end
+    end
+
+    function new_player.draw()
+        love.graphics.setColor(new_player.color)
+        love.graphics.polygon("fill", new_player.vertices())
+        love.graphics.setColor(default_love_color)
+    end
+
+    return new_player
 end
 
-function module.player.draw()
-    love.graphics.setColor(module.player.color)
-    love.graphics.polygon("fill", module.player.vertices())
-    love.graphics.setColor(default_color)
-end
-
-return module
+return player
