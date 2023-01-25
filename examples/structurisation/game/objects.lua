542786a32759ffabee43e0b5966c4ed6f31750b4
suap-dev <suap.dev@gmail.com>
2023-01-25 22:51:29 +0100

    proper ball control

--- examples/structurisation/game/objects.lua
+++ examples/structurisation/game/objects.lua
@@ -1,17 +1,48 @@
 local m = love.physics.getMeter()
 
+local acceleration = 60 * m -- m/s^2, s = 1
+local max_horisontal_velocity = 20 * m -- m/s, s = 1
+
 local base_color = { love.graphics.getColor() }
 local ball = {
     body = love.physics.newBody(WORLD, 2 * m, 2 * m, "dynamic"),
-    shape = love.physics.newCircleShape(1 * m),
+    shape = love.physics.newCircleShape(0.5 * m),
     color = { 0.76, 0.18, 0.05 },
-    draw = function(self)
-        love.graphics.setColor(self.color)
-        love.graphics.circle("fill", self.body:getX(), self.body:getY(), self.shape:getRadius())
-        love.graphics.setColor(base_color)
-    end
 }
+ball.acceleration_force = acceleration / ball.body:getMass()
 ball.fixture = love.physics.newFixture(ball.body, ball.shape)
+ball.fixture:setFriction(1)
+ball.body:setAngularDamping(100)
+function ball.draw(self)
+    love.graphics.setColor(self.color)
+    love.graphics.circle("fill", self.body:getX(), self.body:getY(), self.shape:getRadius())
+    love.graphics.setColor(base_color)
+end
+
+function ball.handle_input(self)
+    if love.keyboard.isDown("right") then
+        if self.body:getLinearVelocity() < max_horisontal_velocity then
+            self.body:applyForce(
+                self.acceleration_force,
+                0
+            )
+        end
+    end
+    if love.keyboard.isDown("left") then
+        if self.body:getLinearVelocity() > -max_horisontal_velocity then
+            self.body:applyForce(
+                -self.acceleration_force,
+                0
+            )
+        end
+    end
+    if love.keyboard.isDown("down") then
+        self.body:applyForce(0, force)
+    end
+    if love.keyboard.isDown("space") then
+
+    end
+end
 
 local floor_height = 1 * m;
 local floor = {
--- examples/structurisation/main_ish.lua
+++ examples/structurisation/main_ish.lua
@@ -7,11 +7,12 @@ WORLD = love.physics.newWorld(0, 9.81 * meter, true)
 local ball = require("examples.structurisation.game.objects").ball
 local floor = require("examples.structurisation.game.objects").floor
 
+function love.update(dt)
+    WORLD:update(dt)
+    ball:handle_input()
+end
+
 function love.draw()
     ball:draw()
     floor:draw()
-end
-
-function love.update(dt)
-    WORLD:update(dt)
 end
