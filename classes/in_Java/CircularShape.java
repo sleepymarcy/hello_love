2e1039de9205665c0866af4c2c9b3aac3f368ab5
sleepymarcy <martyna.sowinska@hotmail.com>
2023-01-26 23:43:45 +0100

    CircularShape and CircularObject

---
+++ classes/in_Java/CircularShape.java
@@ -0,0 +1,20 @@
+// WARNING: This file is auto-generated and any changes to it will be overwritten
+import lang.stride.*;
+import java.util.ArrayList;
+
+/**
+ * What: Circular shape.
+ * Why: So it's easier for us to create specific kind of shape. 
+ * This is identical as Shape but it's not abstract. 
+ * Origin: Copy of a constructor newCircleShape from class Shape from Love2d.
+ */
+public class CircularShape extends Shape
+{
+    /**
+     * Creates circular shape of given radius, using the superclass constructor.
+     */
+    public CircularShape(radius)
+    {
+        super(radius);
+    }
+}
